2cc7484335762ce8c08711297a79ae180e6f77e7
bowloforanges <george.soriano.plus@gmail.com>
2020-03-05 20:12:43 -0600

    added multiantennae support and basic interpolation when value is 0

--- matlab/csi_per_packets.m
+++ matlab/csi_per_packets.m
@@ -1,6 +1,6 @@
 
 %
-% Majorly based on Daniel Halperin's Work, written by Jorge Soriano Cáceres, 2020
+% Majorly based on Daniel Halperin's Work, written by Jorge Soriano CÃ¡ceres, 2020
 %
 
 function ret = csi_per_packets(filename)
@@ -9,24 +9,39 @@ function ret = csi_per_packets(filename)
 
     csi_trace = read_bf_file(filename);
     [m, n] = size(csi_trace);
-    ret = zeros(1,m);
-
     [a,b,c] = size(csi_trace{1}.csi);
+    
+    ret = zeros(m,b);
     
     for i = 1:m
 
         current_csi_entry = csi_trace{i};
         current_csi = get_scaled_csi(current_csi_entry);
-        csi_to_plot = db(abs(squeeze(current_csi)));
+        csi_to_plot = db(abs(squeeze(current_csi).'));
 
-        for j = 1:b
-            
-            ret(j,i) = csi_to_plot(j,1);
-            
+        [x,y] = size(csi_to_plot);
+        
+        %check if antennae used are only 1, if this is the case we must
+        % transpose the matrix in order to use it later.
+        if x == 1 && y == 30
+            csi_to_plot = csi_to_plot.';
+            [x,y] = size(csi_to_plot);
         end
 
+        for j = 1:y            
+            ret(i,j) = csi_to_plot(1,j);            
+        end        
+    end
+    
+    %Interpolate missing packet data (0)
+        %goes sideways in matrix
+    for k = 1:y           
+        %goes from top to bottom in matrix
+        for l = 2:m-1
+            if ret(l,k) == 0          
+                ret(l,k) = (ret(l-1,k) + ret(l+1,k))/2;                
+            end            
+        end            
     end
 
 end
-
-
